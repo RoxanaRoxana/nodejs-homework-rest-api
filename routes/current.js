e26cc20a29aa86a86ac6d9078c12c1749124aeae
RoxanaRoxana <roxana0804@gmail.com>
2022-05-15 20:02:13 +0200

    logout

--- routes/current.js
+++ routes/current.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-
+const User = require("../service/schemas/users");
 const router = Router();
 
 router.get("/current", (req, res) => {
@@ -7,5 +7,13 @@ router.get("/current", (req, res) => {
   res.json({ message: `You are authorized: ${email}` });
 });
 
+router.get("/users/logout", (req, res) => {
+  const user = User.findById({_id: req.user});
+  if (!user) {
+    res.status(401).json({ message: "Not authorized" });
+  } else {
+res.status(200).json("Logged out")
+  }
+});
 
 module.exports = router;
