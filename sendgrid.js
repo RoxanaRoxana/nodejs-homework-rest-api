e7c316950b05cb5ad5fb40d3087fd08fff9943d2
RoxanaRoxana <roxana0804@gmail.com>
2022-06-20 12:26:48 +0200

    fixes

--- sendgrid.js
+++ sendgrid.js
@@ -1,29 +1,34 @@
 const sgMail = require("@sendgrid/mail");
 require("dotenv").config();
-
+const server = process.env.SERVER_LINK;
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 
 const emailConfig = (email, verificationToken) => {
   return {
     to: `${email}`,
     from: "antoninablack666@wp.pl",
     subject: "SG Verification Email",
-    text: `To verify your account please click the following link: http://localhost:3000/api/users/verify/${verificationToken}`,
+    text: `To verify your account please click the following link: ${server}${verificationToken}`,
     html: `<body>
-    <p>To verify your account please click the following link: http://localhost:3000/api/users/verify/${verificationToken}</p>
+    <p>To verify your account please click the following link: ${server}${verificationToken}</p>
     </body>`,
   };
 };
 
+
+
 const sendMail = (email, verificationToken) => {
-  sgMail
-    .send(emailConfig(email, verificationToken))
-    .then(() => {
-      console.log("Verification email sent");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+  return new Promise((resolve, reject) => {
+    sgMail
+      .send(emailConfig(email, verificationToken))
+      .then(() => {
+        resolve("Verification email sent");
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  })
+}
 
 module.exports = { sendMail };
